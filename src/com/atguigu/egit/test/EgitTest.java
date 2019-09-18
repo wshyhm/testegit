9dd17cb0b8bc426caddc2dd52b9bd8f566a04221
MZ <714233302@qq.com>
2019-09-18 20:18:09 +0800

    MZ更新666666

--- src/com/atguigu/egit/test/EgitTest.java
+++ src/com/atguigu/egit/test/EgitTest.java
@@ -6,6 +6,6 @@ public class EgitTest {
 		System.out.println("aaa");
 		System.out.println("bbb");
 		System.out.println("ccc");
-		System.out.println("666");
+		System.out.println("666666");
 	}
 }
